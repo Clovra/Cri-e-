05bc7c98f65ce5cbbab172d62d825dc1d6ab9cc2
Clotaire Ducray <99202774+Clovra@users.noreply.github.com>
2024-01-11 10:42:48 +0100

    la

--- php/listelotday/header.php
+++ php/listelotday/header.php
@@ -1,16 +1,20 @@
 <!DOCTYPE html>
 <html>
- <head>
- <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
- <title>lot</title>
 
- </head>
- <body class=container-fluid>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <title>lot</title>
+</head>
+
+<body class=container-fluid>
     <div class="row">
-        <li class="col">
-        <a aria-current="page" href="index.php?uc=listelot">afficher lot</a>
-        </li>
+
+        <li class="col"> <a aria-current="page" href="index.php?uc=listelot">afficher lot</a> </li>
+
+        <li class="col"> <a aria-current="page" href="index.php?uc=listelot">afficher lot</a> </li>
+
+        <li class="col"> <a aria-current="page" href="index.php?uc=listelot">afficher lot</a> </li>
     </div>
- </body>
+</body>
 </html>
--- php/listelotday/v_listeLot.php
+++ php/listelotday/v_listeLot.php
@@ -8,21 +8,25 @@
 <body>
   <section>
     <div>
-      <h1 class="text-center">vente du <?php echo(date("Y-m-d")) ?></h1>
+      <h1 class="text-center">vente du <?php echo(date("d-m-Y")) ?></h1>
         <table class="table table-bordered">
           <thead>
               <tr>
                   <th>lot</th>
-                  <th>date</th>
+                  <th>Bateau</th>
+                  <th>date péche</th>
                   <th>Qualite</th>
+                  <th>Taille</th>
                   <th>Bac</th>
               </tr>
           </thead>
           <tbody><?php while ($donnees = $req->fetch()){?>
             <tr > 
                 <td> <?php echo htmlspecialchars($donnees['id']); ?> </td>
+                <td> <?php echo htmlspecialchars($donnees['idBateau']); ?> </td>
                 <td> <?php echo htmlspecialchars($donnees['idDatePeche']); ?> </td> 
                 <td> <?php echo htmlspecialchars($donnees['idQualite']); ?> </td>
+                <td> <?php echo htmlspecialchars($donnees['idTaille']); ?> </td>
                 <td> <?php echo htmlspecialchars($donnees['idBac']); ?> </td>
               </tr>
             <?php } $req->closeCursor(); ?>
